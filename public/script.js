f516290e8512d99a2b0163c10e7fec203e1fa372
sukhveer singh <44468231+veersingh9540@users.noreply.github.com>
2020-09-30 06:14:50 +0530

    initial commit

---
+++ public/script.js
@@ -0,0 +1,134 @@
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+let myVideoStream;
+const myVideo = document.createElement('video')
+myVideo.muted = true;
+
+var peer = new Peer(undefined,{
+    path:'/peerjs',
+    host:'/',
+    port:'3030',
+}); 
+
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+  }).then(stream => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+
+    peer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+          addVideoStream(video, userVideoStream)
+        })
+      })
+
+
+    socket.on('user-connected', (userId) => {
+        connectToNewUser(userId,stream)
+      })
+    })
+
+    let text = $('input');
+console.log(text);
+
+    $('html').keydown((e)=>{
+        if(e.which == 13 && text.val().length != 0){
+            
+            socket.emit('message',text.val());
+            text.val('')
+        }
+    });
+
+    socket.on('createMessage',(message) =>{
+        console.log(message);
+        $('ul').append(`<li class="messages"><b>user</b><br/>${message}</li>`);
+        scrollToBottom();
+    })
+
+peer.on('open' , id =>{
+    socket.emit('join-room',RoomId,id);
+})
+
+
+
+
+const connectToNewUser = (userId,stream) =>{
+    const call = peer.call(userId,stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream=>{
+        addVideoStream(video ,userVideoStream)
+    })
+}
+
+const addVideoStream = (video, stream)=>{
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata',()=>{
+        video.play();
+    })
+    videoGrid.append(video);
+}
+
+const  scrollToBottom =() =>{
+    let d = $('.main__chat_window');
+    d.scrollTop(d.prop("scrollHeight"));
+}
+
+// mute the audio /video 
+const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+      myVideoStream.getAudioTracks()[0].enabled = false;
+      setUnmuteButton();
+    } else {
+      setMuteButton();
+      myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+  }
+
+  const playStop = () => {
+    console.log('object')
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+      myVideoStream.getVideoTracks()[0].enabled = false;
+      setPlayVideo()
+    } else {
+      setStopVideo()
+      myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+  }
+  
+  const setMuteButton = () => {
+    const html = `
+      <i class="fas fa-microphone"></i>
+      <span>Mute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+  }
+  
+  const setUnmuteButton = () => {
+    const html = `
+      <i class="unmute fas fa-microphone-slash"></i>
+      <span>Unmute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+  }
+  
+  const setStopVideo = () => {
+    const html = `
+      <i class="playVideo fas fa-video"></i>
+      <span>Stop Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+  }
+  
+  const setPlayVideo = () => {
+    const html = `
+    <i class="stop fas fa-video-slash"></i>
+      <span>Play Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+  }
---
+++ todo.md
@@ -0,0 +1,10 @@
+All tasks to DO 
+- Initialize our NODE JS project DONE
+- Initialize our first view DONE
+- create Room id DONE
+- The ability to view your own video DONE
+- Add ability to allow to stream their videos 
+- add styling 
+- add ability to create message 
+- Add mute button 
+- Add stop video buttton 
